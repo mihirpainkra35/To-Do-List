47331975a8d65d04012dc94485c062b7c5902004
Mihir painkra <mihirpaikra35@gmail.com>
2023-07-07 20:37:57 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -8,17 +8,16 @@ const port = process.env.PORT || 5000
 
 //path
 const staticPath=path.join(__dirname,'../public')
-const viewPath = path.join(__dirname,'../')
+
 // middleware
 app.use(express.static(staticPath))
 app.set('view engine','hbs')
-app.set('views',viewPath)
-// app.set('views')
+app.set('views')
 
 
 app.get('/',(req,res)=>{
 
-res.render('views/index')
+res.render('index')
 
 })
 
